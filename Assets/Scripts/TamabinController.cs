3a13fc88c84c598baeb4c5cb07cd0e308c4334e8
Pedro Henrique <pedro00dk@live.com>
2016-06-25 17:27:11 -0300

    Add a new button go to Tamaguchi scene in the Connector
    A function to load the next scene to was added in the TamabinController script, the Tamaguchi scene too was added to the build list.

--- Assets/Scripts/TamabinController.cs
+++ Assets/Scripts/TamabinController.cs
@@ -1,18 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TamabinController : MonoBehaviour {
 	public string tamabinBluetoothName;
 	TamabinConnector connector;
 	string lastMessage;
+	bool connected;
 
 	void Start() {
+		DontDestroyOnLoad(gameObject);
 		connector = TamabinConnector.getInstance();
 		connector.SetTamabinBluetoothName(tamabinBluetoothName);
 		lastMessage = "";
+		connected = false;
+	}
+
+	public void LoadTamaguichiScene() {
+		if (connected) {
+			lastMessage = "Tamaguchi scene loaded";
+			SceneManager.LoadScene("Tamaguchi");
+		} else {
+			lastMessage = "Cannot start without connect";
+		}
 	}
 
 	public void Connect() {
 		lastMessage = connector.TryConnect();
+		connected = lastMessage == TamabinConnector.CONNECTED || lastMessage == TamabinConnector.ALREADY_CONNECTED;
 	}
 
 	public void Disconnect() {
