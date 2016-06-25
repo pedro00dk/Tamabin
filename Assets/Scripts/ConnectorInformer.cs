183ff46bdc13eac9916f46cb0e48ed4de1f58f53
Pedro Henrique <pedro00dk@live.com>
2016-06-25 16:35:35 -0300

    Create the Connector Scene to test the connection
    This scene can connect and disconnect from the Tamabin device and test if the Tamabin messages are being received.

---
+++ Assets/Scripts/ConnectorInformer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectorInformer : MonoBehaviour {
+	public TamabinController controller;
+	public Text informer;
+
+	void Update() {
+		informer.text = controller.GetLastMessage();
+	}
+}
---
+++ Assets/Scripts/TamabinController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class TamabinController : MonoBehaviour {
+	public string tamabinBluetoothName;
+	TamabinConnector connector;
+	string lastMessage;
+
+	void Start() {
+		connector = TamabinConnector.getInstance();
+		connector.SetTamabinBluetoothName(tamabinBluetoothName);
+		lastMessage = "";
+	}
+
+	public void Connect() {
+		lastMessage = connector.TryConnect();
+	}
+
+	public void Disconnect() {
+		lastMessage = connector.TryDisconnect();
+	}
+
+	public void TamabinCall() {
+		string tamabinMessage = connector.TamabinCall();
+		lastMessage = tamabinMessage != null ? "Eat" : "Not eat";
+	}
+
+	public string GetLastMessage() {
+		return lastMessage;
+	}
+}
