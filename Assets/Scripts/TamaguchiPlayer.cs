6b798a674132c6fb2ca6e2049edf9e483eb4efc8
Pedro Henrique <pedro00dk@live.com>
2016-07-01 12:50:13 -0300

    Creation of the TamaguchiPlayer script
    This scrpit will play the movies.

---
+++ Assets/Scripts/TamaguchiPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TamaguchiPlayer : MonoBehaviour {
+
+	public MovieTexture normal;
+	public MovieTexture sad;
+	public MovieTexture happy;
+	public MovieTexture thank;
+	public MovieTexture fullAndClear;
+
+	TamabinController tamaguchiController;
+
+	void Start() {
+		tamaguchiController = GetComponent<TamaguchiController>();
+	}
+
+	void Update() {
+		
+	}
+}
