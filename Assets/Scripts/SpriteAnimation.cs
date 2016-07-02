493e6b08b2c3f4c25379a8e75b92c871068d191a
Pedro Henrique <pedro00dk@live.com>
2016-07-02 15:39:10 -0300

    Remove the TamaguchiPlayer and create the SpriteAnimation
    Android cannot play movies, sprites will be used in the place.

---
+++ Assets/Scripts/SpriteAnimation.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteAnimation : MonoBehaviour {
+
+	// Components
+	SpriteRenderer spriteRenderer;
+
+	// Internal properties
+	Sprite[] sprites;
+	float framerate;
+	bool loop;
+
+	float startTime;
+	int currentFrame;
+	int executionCount;
+
+	void Start() {
+		sprites = null;
+		framerate = -1;
+		loop = false;
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		startTime = Time.timeSinceLevelLoad;
+		currentFrame = 0;
+		executionCount = 0;
+	}
+
+	void Update() {
+		if (sprites != null && sprites.Length > 0) {
+			if (loop || executionCount == 0) {
+				currentFrame = (int) (((Time.timeSinceLevelLoad - startTime) * framerate) % sprites.Length);
+				spriteRenderer.sprite = sprites[currentFrame];
+				if (currentFrame == sprites.Length - 1) {
+					executionCount++;
+				}
+			}
+		}
+	}
+
+	public void SetSpritesAndStartAnimation(Sprite[] sprites, float framerate, bool loop) {
+		this.sprites = sprites;
+		this.framerate = framerate;
+		this.loop = loop;
+		currentFrame = 0;
+		executionCount = 0;
+	}
+}
