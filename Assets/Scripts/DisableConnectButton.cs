fee2d8ddaaed6bce0733ff9b69e4c268e6827ac3
Pedro Henrique <pedro00dk@live.com>
2016-07-09 13:48:27 -0300

    Create script to disable the connect button when connected

---
+++ Assets/Scripts/DisableConnectButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class DisableConnectButton : MonoBehaviour {
+
+	Button button;
+	Image buttonImage;
+
+	TamabinController controller;
+
+	void Start () {
+		button = GetComponent<Button>();
+		buttonImage = GetComponent<Image>();
+		controller = FindObjectOfType<TamabinController>();
+	}
+
+	void Update () {
+		if (controller.Connected()) {
+			button.enabled = false;
+			buttonImage.enabled = false;
+		} else {
+			button.enabled = true;
+			buttonImage.enabled = true;
+		}
+	}
+}
